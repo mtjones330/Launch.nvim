1ef06c6a4d8b459c6f06b46c7ef98be6a0166163
Matthew Jones <14156163+mtjones330@users.noreply.github.com>
2024-05-07 20:36:06 -0400

    Add fast exit insert mode

--- lua/user/mtjones330/keymaps.lua
+++ lua/user/mtjones330/keymaps.lua
@@ -2,3 +2,9 @@ local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
+
+-- Press kj fast to exit Insert mode
+keymap("i", "kj", "<ESC>", opts)
+
+-- Press kj fast to exit Terminal mode
+keymap("t", "kj", "<C-\\><C-n>", opts)
